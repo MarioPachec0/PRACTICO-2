4758bc1e5c81f88f29f88e9f969ef3350b00c271
mariopachec0 <m4ri0723@gmail.com>
2024-03-28 10:22:30 -0400

    practico 2

---
+++ SistemaVentas/SistemaVentasVISTAS2/BienvenidosVISTAS/BienvenidosVistas.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SistemaVentasVISTAS2.BienvenidosVISTAS
+{
+    partial class BienvenidosVistas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Font = new Font("Showcard Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.Window;
+            button1.Location = new Point(503, 439);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 49);
+            button1.TabIndex = 11;
+            button1.Text = "Iniciar Sesión";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.Font = new Font("Bauhaus 93", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.Window;
+            label3.Location = new Point(45, 439);
+            label3.Name = "label3";
+            label3.Size = new Size(123, 23);
+            label3.TabIndex = 10;
+            label3.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ActiveCaptionText;
+            label2.Font = new Font("Bauhaus 93", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.Window;
+            label2.Location = new Point(45, 371);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 23);
+            label2.TabIndex = 9;
+            label2.Text = "Usuario";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(217, 371);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(215, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ScrollBar;
+            textBox2.Location = new Point(217, 439);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(215, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // BienvenidosVistas
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            BackgroundImage = Properties.Resources.Captura_de_pantalla_2024_03_26_095104;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(667, 501);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            DoubleBuffered = true;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "BienvenidosVistas";
+            Text = "BienvenidosVistas";
+            Load += BienvenidosVistas_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ SistemaVentas/SistemasVentas.DAL/UsuarioDAL.cs
@@ -0,0 +1,44 @@
+﻿using DAL;
+using SistemasVentas.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DAL
+{
+    public class UsuarioDAL
+    {
+        public DataTable ListarUsuariosDal()
+        {
+            string consulta = "select * from usuario";
+            DataTable lista = conexion.EjecutarDataTabla(consulta, "tabla");
+            return lista;
+        }
+        public void InsertarUsuarioDal(Usuario usuario)
+        {
+            string consulta = "insert into usuario values (" + usuario.IdPersona + "," +
+                                                          "'" + usuario.NombreUser + "'," +
+                                                          "'" + usuario.Contraeña + "'," +
+                                                          "'" + usuario.Fecha + "'," +
+                                                          "'Activo')";
+            conexion.Ejecutar(consulta);
+
+        }
+
+        public DataTable UsuarioDatosDal();
+       {
+          string consulta = "SELECT USUARIO.IDUSUARIO, (PERSONA.APELLIDO+' '+PERSONA.NOMBRE)NOMBRECOMPLETO, PERSONA.TELEFONO, USUARIO.IDUSUARIO, USUARIO.NOMBREUSER , 
+                            "ROL.NOMBRE AS NOMBREROL, USUARIOROL.FECHAASIGNA "+
+                            "FROM PERSONA INNER JOIN"+
+                 "USUARIO ON PERSONA.IDPERSONA = USUARIO.IDPERSONA INNER JOIN"+
+                  "USUARIOROL ON USUARIO.IDUSUARIO = USUARIOROL.IDUSUARIO INNER JOIN"+
+                 "ROL ON USUARIOROL.IDROL = ROL.IDROL";
+          return conexion.EjecutarDataTabla(colsulta, "fdgf")
+        }
+    }   
+}
